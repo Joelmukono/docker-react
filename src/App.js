a3c70bb2340055185c3252b18237359edd4fc272
Joel Mukono <joel@nanasi.co>
2021-06-17 20:58:25 +0300

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. a b c d e f
+          Edit <code>src/App.js</code> and save to         test to see html
         </p>
         <a
           className="App-link"
